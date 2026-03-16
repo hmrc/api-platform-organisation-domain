3b9aef8f4facf80bfebf3a3a5bb90d628cb61c97
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2026-03-16 14:55:04 +0000

    APIS-8031 Add new organisation allow list

---
+++ api-platform-organisation-domain-test/src/test/scala/uk/gov/hmrc/apiplatform/modules/organisations/submissions/domain/models/OrganisationAllowListSpec.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apiplatform.modules.organisations.submissions.domain.models
+
+import play.api.libs.json.Json
+import uk.gov.hmrc.apiplatform.modules.common.domain.models.UserId
+import uk.gov.hmrc.apiplatform.modules.common.utils.{BaseJsonFormattersSpec, FixedClock}
+
+import uk.gov.hmrc.apiplatform.modules.organisations.domain.models.OrganisationName
+
+class OrganisationAllowListSpec extends BaseJsonFormattersSpec with FixedClock {
+
+  def jsonOrganisationAllowList(userId: UserId, organisationName: OrganisationName) = {
+    s"""{
+       |  "userId" : "${userId.value.toString()}",
+       |  "organisationName" : "${organisationName.value}",
+       |  "requestedBy" : "bob@example.com",
+       |  "requestedOn" : "2020-01-02T03:04:05.006Z"
+       |}""".stripMargin
+  }
+
+  val userId  = UserId.random
+  val orgName = OrganisationName("My org")
+
+  val organisationAllowList = OrganisationAllowList(
+    userId,
+    orgName,
+    "bob@example.com",
+    instant
+  )
+
+  "OrganisationAllowList" should {
+    "convert to json" in {
+      Json.prettyPrint(Json.toJson[OrganisationAllowList](organisationAllowList)) shouldBe jsonOrganisationAllowList(userId, orgName)
+    }
+
+    "read from json" in {
+      testFromJson[OrganisationAllowList](jsonOrganisationAllowList(userId, orgName))(organisationAllowList)
+    }
+  }
+}
